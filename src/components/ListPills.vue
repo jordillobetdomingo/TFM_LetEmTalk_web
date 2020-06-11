be01ae6fe2b9590813209b1d56d89b9018223018
jordi.llobetdomingo <jordi.llobetdomingo@students.salle.url.edu>
2020-06-11 20:04:55 +0200

    Show PillForm in Add and Edit Pill

--- src/components/ListPills.vue
+++ src/components/ListPills.vue
@@ -3,14 +3,32 @@
         <div v-for="pill in listPills" :key="pill.pillId">
             <p> {{pill.text}}</p>
             <p> Author: {{ pill.firstNameAuthor + ' ' + pill.lastNameAuthor }}</p>
-            <p v-if="pill.canEdit"> Edit and delete buttons </p>
+            <input type="button" v-if="pill.canEdit" value="EditPill" @click="showForm(pill)">
         </div>
+        <FormPill v-if="showFormPill" :pill="pillSelected"></FormPill>
     </div>
 </template>
 
 <script>
+import FormPill from '@/components/FormPill'
+
 export default {
     name: 'listPills',
-    props: ['listPills']
+    props: ['listPills'],
+    components: {
+        FormPill
+    },
+    data() {
+        return {
+            showFormPill: false,
+            pillSelected: null
+        }
+    },
+    methods: {
+        showForm(pill) {
+            this.pillSelected = pill;
+            this.showFormPill = true;
+        } 
+    }
 }
 </script>
--- src/views/Issue.vue
+++ src/views/Issue.vue
@@ -2,8 +2,10 @@
     <div id="issue">
         <router-link :to="'/room/' + issueWithPills.issue.roomId + '/'"> Back to the room </router-link>
         <h1>{{issueWithPills.issue.title}}</h1>
-        <p v-if="issueWithPills.issue.canEdit"> Button of edit and delete issue</p>
-        <p v-if="issueWithPills.issue.canAddPill"> Button of add pill </p>
+        <input type="button" v-if="issueWithPills.issue.canEdit" value="EditIssue">
+        <input type="button" v-if="issueWithPills.issue.canEdit" value="DeleteIssue">
+        <input v-if="issueWithPills.issue.canAddPill" type="button" value="AddPill" @click="showFormAddPill = true;">
+        <FormPill v-if="this.showFormAddPill" :pill="{'issueId': issueWithPills.issue.issueId, 'text': '', 'authorId': null}"></FormPill>
         <ListPills :listPills="issueWithPills.pills"> </ListPills>
     </div>
 </template>
@@ -11,16 +13,19 @@
 <script>
 import axios from '@/utils/axios-instance'
 import ListPills from '@/components/ListPills'
+import FormPill from '@/components/FormPill'
 
 export default {
     name: 'Issue',
     data() {
         return {
-            issueWithPills: {issue: {title: '', roomId: '0'}, pills: []}
+            issueWithPills: {issue: {title: '', roomId: '0'}, pills: []},
+            showFormAddPill: false
         }
     },
     components: {
-        ListPills
+        ListPills,
+        FormPill
     },
     methods: {
         loadIssueWithPills() {
@@ -28,6 +33,9 @@ export default {
                 .then(response => {
                     this.issueWithPills = response.data
                 });            
+        },
+        showForm() {
+            this.showFormAddPill = true;
         }
     },
     mounted() {
